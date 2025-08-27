9275b25905a03b6629c213a052c4ca738d900acb
Pedro Moreno <pm305567@gmail.com>
2025-08-27 15:58:53 -0300

    feat: cria o CRUD de autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { FilmesModule } from './filmes/filmes.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [FilmesModule, PrismaModule],
-  controllers: [],
-  providers: [],
+  imports: [FilmesModule, PrismaModule, AuthModule],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AppModule {}
